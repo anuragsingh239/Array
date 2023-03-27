6fee37386f8297b6dcd4d60100c7167375c5b056
Anurag Singh <anurag.singh2_cs20@gla.ac.in>
2023-03-27 18:53:39 +0530

    Initial commit

--- src/GreaterElementInArray.java
+++ src/GreaterElementInArray.java
@@ -1,20 +1,21 @@
-//import java.util.Scanner;
-//
-//public class GreaterElementInArray {
-//    public static void main(String[] args) {
-//        Scanner sc=new Scanner(System.in);
-//        System.out.print("Enter the size of array : ");
-//        int size=sc.nextInt();
-//        int []array=new int[size];
-//        System.out.print("Enter the element : ");
-//        for(int i=0;i<size;i++){
-//            array[i]= sc.nextInt();
-//        }
-//        int max=array[0];
-//        for (int i=0;i<size;i++){
-//            if(array[i]>max){
-//
-//            }
-//        }
-//    }
-//}
+import java.util.Scanner;
+
+public class GreaterElementInArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size=sc.nextInt();
+        int []array=new int[size];
+        System.out.print("Enter the element : ");
+        for(int i=0;i<size;i++){
+            array[i]= sc.nextInt();
+        }
+        int max=array[0];
+        for (int i=0;i<size;i++){
+            if(array[i]>max){
+                max=array[i];
+
+            }
+        }return;
+    }
+}
